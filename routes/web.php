9e78da6f8b8600ef900c139e5ab65cc678a81ae9
Jake Meyers <jakermeyers92@gmail.com>
2023-11-16 20:07:37 -0600

    create feed

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 // Route::statamic('example', 'example-view', [
 //    'title' => 'Example'
 // ]);
+
+Route::statamic('feed', 'layouts/feed', [
+    'layout' => 'layouts/feed',
+    'content_type' => 'atom'
+]);
